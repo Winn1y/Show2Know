b29177dfff23b0496484f6d4f5cbc8c6ce906057
Venchili <953692627@qq.com>
2023-08-18 17:04:01 +0800

    choose colormap.md & choose colormap.py

---
+++ 0 Collection and Color/Choose Colormap.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from colorspacious import cspace_converter
+
+## 准备工作
+cmaps = {} # 创建一个空的字典 cmaps，用于存储不同类型颜色映射的列表
+
+gradient = np.linspace(0, 1, 256) # 创建一个256个点的渐变，这将用于显示颜色映射的效果。
+gradient = np.vstack((gradient, gradient))# 通过垂直堆叠这两行，得到一个新的数组，形状为 (4, 256)。
+
+
+def plot_color_gradients(category, cmap_list):
+    # Create figure and adjust figure height to number of colormaps
+    nrows = len(cmap_list) # 计算需要绘制的行数，即颜色映射的数量
+    figh = 0.35 + 0.15 + (nrows + (nrows - 1) * 0.1) * 0.22 # 计算图形的高度，根据行数调整高度，使图形排列紧凑
+    fig, axs = plt.subplots(nrows=nrows + 1, figsize=(6.4, figh))# 创建图形和子图，设置图形的大小和布局
+    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh,
+                        left=0.2, right=0.99)
+    axs[0].set_title(f'{category} colormaps', fontsize=14)
+
+    for ax, name in zip(axs, cmap_list): # 遍历每个子图和颜色映射名称，绘制渐变效果和名称
+        ax.imshow(gradient, aspect='auto', cmap=mpl.colormaps[name])
+        ax.text(-0.01, 0.5, name, va='center', ha='right', fontsize=10,
+                transform=ax.transAxes)
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axs:
+        ax.set_axis_off()
+
+    # Save colormap list for later.
+    cmaps[category] = cmap_list
+
+## Sequential
+plot_color_gradients('Perceptually Uniform Sequential',
+                     ['viridis', 'plasma', 'inferno', 'magma', 'cividis'])
+
+plot_color_gradients('Sequential',
+                     ['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+                      'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+                      'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn'])
+
+plot_color_gradients('Sequential (2)',
+                     ['binary', 'gist_yarg', 'gist_gray', 'gray', 'bone',
+                      'pink', 'spring', 'summer', 'autumn', 'winter', 'cool',
+                      'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper'])
+
+## Diverging
+plot_color_gradients('Diverging',
+                     ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
+                      'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'])
+
+## Cyclic
+plot_color_gradients('Cyclic', ['twilight', 'twilight_shifted', 'hsv'])
+
+## Qualitative
+plot_color_gradients('Qualitative',
+                     ['Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2',
+                      'Set1', 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b',
+                      'tab20c'])
+
+## Miscellaneous一些有特定场景的
+plot_color_gradients('Miscellaneous',
+                     ['flag', 'prism', 'ocean', 'gist_earth', 'terrain',
+                      'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap',
+                      'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet',
+                      'turbo', 'nipy_spectral', 'gist_ncar'])
+
+plt.show()
