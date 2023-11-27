a424ce7e53b89782d4fd67d34cc294c8b681bd88
YasmineLiu <2757225380@qq.com>
2023-11-27 13:04:54 +0800

    “更新”

---
+++ 7 Sankey Plot/sankeyExample.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import plotly.graph_objects as go
+import json, urllib
+import urllib.request
+
+# 1. 使用plotly来绘制sankey
+url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
+response = urllib.request.urlopen(url)
+data = json.loads(response.read())
+
+# override gray link colors with 'source' colors
+opacity = 0.4
+# change 'magenta' to its 'rgba' value to add opacity
+data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
+data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
+                                    for src in data['data'][0]['link']['source']]
+
+fig = go.Figure(data=[go.Sankey(
+    valueformat = ".0f",
+    valuesuffix = "TWh",
+    # Define nodes
+    node = dict(
+      pad = 10,
+      thickness = 30,
+      line = dict(color = "black", width = 0.5),
+      label =  data['data'][0]['node']['label'],
+      color =  data['data'][0]['node']['color']
+    ),
+    # Add links
+    link = dict(
+      source =  data['data'][0]['link']['source'],
+      target =  data['data'][0]['link']['target'],
+      value =  data['data'][0]['link']['value'],
+      label =  data['data'][0]['link']['label'],
+      color =  data['data'][0]['link']['color']
+))])
+
+fig.update_layout(title_text="Energy forecast for 2050<br>Source: Department of Energy & Climate Change, Tom Counsell via <a href='https://bost.ocks.org/mike/sankey/'>Mike Bostock</a>",
+                  font_size=10)
+fig.show()
